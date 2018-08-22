d24f710fd818f9328465ffff25217d2aa7d83067
Victor <mirfaiziev.victor@gmail.com>
2018-08-23 00:33:35 +0200

    added widget customization

--- functions.php
+++ functions.php
@@ -1,5 +1,47 @@
 <?php
+/* ========================================================================================= */
+function apollo_widgets_init()
+{
+    // unregisterd sidebar from parent theme
+    unregister_sidebar('sidebar-1');
 
+    // initialize same sidebar but with another class
+    register_sidebar(
+         array(
+             'name'          => __( 'Main Sidebar', 'apollo' ),
+             'id'            => 'sidebar-1',
+             'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+             'after_widget'  => '</aside>',
+             'before_title'  => '<h3 class="widget-title text-primary">',
+             'after_title'   => '</h3>',
+         )
+    );
+
+}
+add_action( 'widgets_init', 'apollo_widgets_init', 11);
+
+/* ========================================================================================= */
+function remove_specific_widget( $sidebars_widgets ) {
+
+    foreach( $sidebars_widgets as $widget_area => $widget_list ){
+
+        foreach( $widget_list as $pos => $widget_id ){
+
+            if ( $widget_id == 'search-2'){
+                unset( $sidebars_widgets[$widget_area][$pos] );
+            }
+        }
+    }
+
+    return $sidebars_widgets;
+}
+
+add_filter( 'sidebars_widgets', 'remove_specific_widget' );
+
+
+
+
+/* ========================================================================================= */
 function theme_name_scripts(){
     wp_deregister_script('jquery');
     wp_enqueue_script('jquery-slim', get_stylesheet_directory_uri() . '/assets/js/jquery-3.2.1.slim.min.js', array(), '3.2.1');
