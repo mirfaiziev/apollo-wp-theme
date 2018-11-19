f4a02cf8ea56c3dd386f4b31dbfd4e1061243084
moleg <mirfaizievoleg@gmail.com>
2018-11-19 20:21:24 +0200

    Adaptive header

--- footer.php
+++ footer.php
@@ -5,63 +5,32 @@
  * Contains the closing of the id=main div and all content after
  *
  * @package WordPress
- * @subpackage Apollo
- * @since Apollo 0.1
+ * @subpackage Twenty_Eleven
+ * @since Twenty Eleven 1.0
  */
 ?>
 
-<!-- </div>.container -->
-</div><!-- #main -->
+	</div><!-- #main -->
 
-<footer id="colophon" role="contentinfo">
-	<div class="row">
+	<footer id="colophon" role="contentinfo">
 		
 
-		<?php
+			<?php
 				/*
 				 * A sidebar in the footer? Yep. You can customize
 				 * your footer with three columns of widgets.
 				 */
-				if ( ! is_404() ) {
-					get_sidebar( 'footer' );
-				}
-				?>		
+			if ( ! is_404() ) {
+				get_sidebar( 'footer' );
+			}
+			?>
+
+			
+		
+	</footer><!-- #colophon -->
 
 
-				<div id = "supplementary" class="col">
-					
-					<h3 class = "widget-title text-light">Наші реквізити</h3>
-					<p class = "text-light">Україна,
-						ул. Старовокзальна, 17, <br>
-						м. Київ, 01032
-					</p>
-					
-						<p class = "text-light "> e-mail: <br>
-							med@apollo.kiev.ua,<br>
-							ldc@apollo.kiev.ua
-						</p>
-					</div>
+<?php wp_footer(); ?>
 
-				<div id = "supplementary" class="col">
-					<h3 class=" widget-title text-light ">Работа с клиентами</h3>
-					<p class = "text-light ">	
-						тел./факс:<br> 
-						+38(044) 235-7829,<br>
-						+38(044) 236-7959.</p>
-						<p class = "text-light ">
-							пн.-пт. з 8-00 год. до 20-00 год.
-						</p>				
-						
-				</div>
-				
-			</div><!-- #page -->
-</footer><!-- #colophon -->
-
-			<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-			<script>window.jQuery || document.write('<script src="vendor/jquery-3.2.1.slim.min.js"><\/script>')</script>
-
-
-			<?php wp_footer(); ?>
-
-		</body>
-		</html>
+</body>
+</html>
--- header.php
+++ header.php
@@ -5,134 +5,172 @@
  * Displays all of the <head> section and everything up till <div id="main">.
  *
  * @package WordPress
- * @subpackage Apollo
- * @since Apollo 0.1
+ * @subpackage Twenty_Eleven
+ * @since Twenty Eleven 1.0
  */
-?>
-<!DOCTYPE html>
-
+?><!DOCTYPE html>
+<!--[if IE 6]>
+<html id="ie6" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 7]>
+<html id="ie7" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html id="ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 6) & !(IE 7) & !(IE 8)]><!-->
 <html <?php language_attributes(); ?>>
-
+<!--<![endif]-->
 <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>" />
-    <meta name="viewport" content="width=device-width" />
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name="viewport" content="width=device-width" />
+<title>
+<?php
+	// Print the <title> tag based on what is being viewed.
+	global $page, $paged;
 
+	wp_title( '|', true, 'right' );
 
-    <title>
-        <?php
-        // Print the <title> tag based on what is being viewed.
-        global $page, $paged;
-        wp_title( '|', true, 'right' );
-        // Add the blog name.
-        bloginfo( 'name' );
-        // Add the blog description for the home/front page.
-        $site_description = get_bloginfo( 'description', 'display' );
-        if ( $site_description && ( is_home() || is_front_page() ) ) {
-            echo " | $site_description";
-        }
-        ?>
-    </title>
+	// Add the blog name.
+	bloginfo( 'name' );
 
-    <link rel="profile" href="http://gmpg.org/xfn/11" />
-    <link rel="pingback" href="<?php echo  get_stylesheet_directory_uri(); ?>>" />
+	// Add the blog description for the home/front page.
+	$site_description = get_bloginfo( 'description', 'display' );
+if ( $site_description && ( is_home() || is_front_page() ) ) {
+	echo " | $site_description";
+}
 
-    <?php
-    /*
-     * We add some JavaScript to pages with the comment form
-     * to support sites with threaded comments (when in use).
-     */
-    if ( is_singular() && get_option( 'thread_comments' ) ) {
-        wp_enqueue_script( 'comment-reply' );
-    }
-    /*
-     * Always have wp_head() just before the closing </head>
-     * tag of your theme, or you will break many plugins, which
-     * generally use this hook to add elements to <head> such
-     * as styles, scripts, and meta tags.
-     */
-    wp_head();
-    ?>
+	// Add a page number if necessary:
+if ( ( $paged >= 2 || $page >= 2 ) && ! is_404() ) {
+	echo esc_html( ' | ' . sprintf( __( 'Page %s', 'twentyeleven' ), max( $paged, $page ) ) );
+}
 
+	?>
+	</title>
+<link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<!--[if lt IE 9]>
+<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
+<![endif]-->
+<?php
+	/*
+	 * We add some JavaScript to pages with the comment form
+	 * to support sites with threaded comments (when in use).
+	 */
+if ( is_singular() && get_option( 'thread_comments' ) ) {
+	wp_enqueue_script( 'comment-reply' );
+}
 
+	/*
+	 * Always have wp_head() just before the closing </head>
+	 * tag of your theme, or you will break many plugins, which
+	 * generally use this hook to add elements to <head> such
+	 * as styles, scripts, and meta tags.
+	 */
+	wp_head();
+?>
 </head>
 
 <body <?php body_class(); ?>>
-<div class="container">
+<div id="page" class="hfeed">
+	<header id="branding" role="banner">
+			<hgroup>
+				<h1 id="site-title"><span><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></span></h1>
+				<h2 id="site-description"><?php bloginfo( 'description' ); ?></h2>
+			</hgroup>
+<div class="row">
+					<div class="col-6">
+			<?php
+				// Check to see if the header image has been removed
+				$header_image = get_header_image();
+			if ( $header_image ) :
+				// Compatibility with versions of WordPress prior to 3.4.
+				if ( function_exists( 'get_custom_header' ) ) {
+					/*
+					 * We need to figure out what the minimum width should be for our featured image.
+					 * This result would be the suggested width if the theme were to implement flexible widths.
+					 */
+					$header_image_width = get_theme_support( 'custom-header', 'width' );
+				} else {
+					$header_image_width = HEADER_IMAGE_WIDTH;
+				}
+				?>
+				
 
-    <div class="header">
+			<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+				<?php
+				/*
+				 * The header image.
+				 * Check if this is a post or page, if it has a thumbnail, and if it's a big one
+				 */
+				if ( is_singular() && has_post_thumbnail( $post->ID ) &&
+						( /* $src, $width, $height */ $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), array( $header_image_width, $header_image_width ) ) ) &&
+						$image[1] >= $header_image_width ) :
+					// Houston, we have a new header image!
+					echo get_the_post_thumbnail( $post->ID, 'post-thumbnail' );
+				else :
+					// Compatibility with versions of WordPress prior to 3.4.
+					if ( function_exists( 'get_custom_header' ) ) {
+						$header_image_width  = get_custom_header()->width;
+						$header_image_height = get_custom_header()->height;
+					} else {
+						$header_image_width  = HEADER_IMAGE_WIDTH;
+						$header_image_height = HEADER_IMAGE_HEIGHT;
+					}
+					?>
+					<img src="<?php header_image(); ?>" width="<?php echo esc_attr( $header_image_width ); ?>" height="<?php echo esc_attr( $header_image_height ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" />
+				<?php endif; // end check for featured image or standard header ?>
+			</a>
 
-        <div class="row">
-            <div class="col-md-3" ><br><img src="<?php echo  get_stylesheet_directory_uri(); ?>/images/logo_apollo.png" class="img-fluid" alt="Responsive image">
-            </div>
+			<?php endif; // end check for removed header image ?>
+</div>
+
+<div class="col-6">
+	<h1>АПОЛЛО</h1>
+	<h3>ЛІКУВАЛЬНО-ДІАГНОСТИЧНИЙ ЦЕНТР</h3>
+	<!-- <br> -->
+	<p><span>&#9990;</span> +38(044) 235-7829,<br>
+	<span>&#9990;</span> +38(044) 236-7959.<br>
+	ул. Старовокзальна, 17, м. Київ, Україна, 01032<br>
+	Режим роботи: пн.-пт. з 8-00 год. до 20-00 год.</p>
+</div>
+
+<div class="clear"></div>
+<div class = "slogan">
+<h3 id = "cursive"><i>28 РОКІВ ПІКЛУЄМОСЯ ПРО ВАШЕ ЗДОРОВ'Я!</i>
+	
+	</h3>
+</div>
+</div>
+			<!-- <?php
+				// Has the text been hidden?
+			if ( 'blank' == get_header_textcolor() ) :
+				$header_image_class = '';
+				if ( $header_image ) {
+					$header_image_class = ' with-image';
+				}
+			?>
+			<div class="only-search<?php echo $header_image_class; ?>">
+			<?php get_search_form(); ?>
+			</div>
+			<?php
+				else :
+			?>
+				<?php get_search_form(); ?>
+			<?php endif; ?> -->
+
+			<nav id="access" role="navigation">
+				<h3 class="assistive-text"><?php _e( 'Main menu', 'twentyeleven' ); ?></h3>
+				<?php /* Allow screen readers / text browsers to skip the navigation menu and get right to the good stuff. */ ?>
+				<div class="skip-link"><a class="assistive-text" href="#content"><?php _e( 'Skip to primary content', 'twentyeleven' ); ?></a></div>
+				<?php if ( ! is_singular() ) : ?>
+					<div class="skip-link"><a class="assistive-text" href="#secondary"><?php _e( 'Skip to secondary content', 'twentyeleven' ); ?></a></div>
+				<?php endif; ?>
+				<?php /* Our navigation menu. If one isn't filled out, wp_nav_menu falls back to wp_page_menu. The menu assigned to the primary location is the one used. If one isn't assigned, the menu with the lowest ID is used. */ ?>
+				<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+			</nav><!-- #access -->		
+	</header><!-- #branding -->
 
 
-
-
-            <div class="col-md-9" ><br>
-                <h1 class ="text-primary text-right font-weight-bold" >ЛІКУВАЛЬНО-ДІАГНОСТИЧНИЙ ЦЕНТР «АПОЛЛО»</h1><br>
-                <p class = "text-right">
-                    Ліцензія МОЗ серія АГ №570585.<br>
-                    ул. Старовокзальна, 17, м. Київ, 01032.<br>
-                    тел./факс: +38(044) 235-7829,<br>
-                    +38(044) 236-7959.<br>
-                    e-mail:  med@apollo.kiev.ua,<br>
-                    ldc@apollo.kiev.ua.<br>      <br>
-                    Режим роботи: пн.-пт. з 8-00 год. до 20-00 год.
-                </p>
-            </div>
-
-
-        </div><!-- row -->
-
-
-        <div class="row" >
-            <a href="<?php echo esc_url( home_url( '/' ) ); ?>">
-
-                <div class="col" style="background: url('<?php echo  get_stylesheet_directory_uri(); ?>/images/headers/med-thumbnail.jpg') no-repeat; background-size: 100%;">
-                    <div class="col" id = "we" align = "right">
-                        <br>
-                        <br>
-                        <br>
-                        <p><img src="<?php echo  get_stylesheet_directory_uri(); ?>/images/apollo.png" class="img-fluid" alt="" width="30%" alt="Responsive image"></p>
-                        <h3 class = "text-primary">Мы позаботимся </h3>
-                        <h3 class = "text-primary">о Вашем здоровье</h3>
-
-                        <h5 class = "text-primary">Аполло предоставляет полный спектр</h5>
-                        <h5 class = "text-primary">медицинского обслуживания</h5>
-                        <br>
-                        <br>
-                    </div>
-                </div>
-
-            </a>
-        </div>
-
-    </div><!-- </header> -->
-
-
-    <nav role="navigation" class = "navbar navbar-expand-lg navbar-light bg-primary border-primary sticky-top border-top ">
-        <button
-                class="navbar-toggler"
-                type="button"
-                data-toggle="collapse"
-                data-target="#navbarSupportedContent"
-                area-label="Toggel navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <?php /* Our navigation menu. If one isn't filled out, wp_nav_menu falls back to wp_page_menu. The menu assigned to the primary location is the one used. If one isn't assigned, the menu with the lowest ID is used. */ ?>
-
-        <?php wp_nav_menu( array(
-                               // 'theme_location' => 'primary',
-                               'container_class' => 'navbar-collapse collapse ',
-                               'container_id' => 'navbarSupportedContent',
-                               'menu_class' => 'navbar-nav mr-auto',
-                           ) ); ?>
-
-        <form method="get" id="searchform" action="/">
-            <label for="s" class="assistive-text">Search</label>
-            <input class="field" name="s" id="s" placeholder="Поиск по сайту" type="text">
-            <input class="submit" name="submit" id="searchsubmit" value="Поиск" type="submit">
-        </form>
-    </nav>
-
-    <div id="main">
+	<div id="main">
